87983c5cb9da9c529df20e66726d9d5df75bf984
Steven Newton <stevennewton0701@gmail.com>
2022-09-23 11:37:21 -0500

    added Bryan Changes and fixed some minor issues

--- js/main.js
+++ js/main.js
@@ -1,29 +1,53 @@
 
 window.addEventListener("load", function(){
-    barra.innerHTML += `
-    <nav class="navbar navbar-expand-lg ">  
-    <a class="navbar-brand" href="./index.html">EcoInclusive</a>
-    
-      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-        <li class="nav-item">
-            <a class="nav-link" href="./products.html">productos</a>
-        </li>
+  barra.innerHTML += `
+ <header class="sticky-top headerA"> 
+  <nav class="navbar navbar-expand-lg navbar-light   ">
+      <a  class="navbar-brand" href="../pages/index.html"><img class="ecoinclusive" src="../src/logoei.png" alt="" srcset=""></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ol class="navbar-nav ml-auto ">
+          <li class="nav-item">
+            <a class="nav-link" href="./products.html">Productos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./carrito.html">Carrito de compras</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="./login.html">Inicio de sesión</a>
+           </li>
+        </ol>
+      </div>
+  </nav>
+</header><hr>`
+;
 
-      </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><a href="./login.html"><i class="bi bi-person-circle"></i>Login</a></button>
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><a href="./carrito.html"><i class="bi bi-cart4"></i>Cart</a></button>
-      </form>
-  </nav><hr>`
-  ;
+barra2.innerHTML +=`
+<hr>
+    <div class="container text-center ">
+       <ul class="list-inline">
+              <li class="list-inline-item"><a style="color:black;" href="./about.html">Nosotros</a></li>
+              <li class="list-inline-item"><a style="color:black;" href="./donaciones.html">Donaciones</a></li>
+              <li class="list-inline-item"><a style="color:black;" href="./contact.html">Contacto</a></li>
+          </ul>
+           <small>Copyright &copy; JavaMonos</small>
+      </div> `
+});
 
-  barra2.innerHTML +=`
-      <div class="container text-center ">
-         <ul class="list-inline">
-                <li class="list-inline-item"><a style="color:black;" href="./about.html">About us</a></li>
-                <li class="list-inline-item"><a style="color:black;" href="./donaciones.html">Donations</a></li>
-                <li class="list-inline-item"><a style="color:black;" href="./contact.html">Contact</a></li>
-            </ul>
-             <small>Copyright &copy; JavaMonos</small>
-        </div> `
+$(document).ready(function () {
+
+$('.first-button').on('click', function () {
+
+  $('.animated-icon1').toggleClass('open');
+});
+$('.second-button').on('click', function () {
+
+  $('.animated-icon2').toggleClass('open');
+});
+$('.third-button').on('click', function () {
+
+  $('.animated-icon3').toggleClass('open');
+});
 });
