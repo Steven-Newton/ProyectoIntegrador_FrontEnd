b2217b0ff26f8a9c3e3bd1baf9da0393428fd541
Steven Newton <stevennewton0701@gmail.com>
2022-10-10 10:10:54 -0500

    form fix

--- js/formulario.js
+++ js/formulario.js
@@ -1,132 +1,21 @@
-const signInBtn = document.getElementById("signIn");
-const signInB = document.getElementById("realSignIn");
-const signUpBtn = document.getElementById("realSignUp");
-const Password = document.getElementById("pass");
-const RepeatPassword = document.getElementById("pass2");
-const Name = document.getElementById("nombre");
-const Phone = document.getElementById("tel");
-const Email = document.getElementById("email");
-const signUpB = document.getElementById("signUp");
-const fistForm = document.getElementById("form1");
-const secondForm = document.getElementById("form2");
-const containerc = document.querySelector(".containerc");
-Name.value="",Phone.value="",email.value="",RepeatPassword.value="",Password.value="";
-
-
-let campoNombre = document.getElementById("nombre");
-let campoTel = document.getElementById("tel");
-let campoEmail = document.getElementById("email");
-let campoContraseña = document.getElementById("pass");
-let validaContraseña = document.getElementById("pass2"); 
-
-
-fistForm.addEventListener("submit", (e) => e.preventDefault());
-secondForm.addEventListener("submit", (e) => e.preventDefault());
-
-signUpBtn.addEventListener("click", function(e){
-	e.preventDefault();
-    ValidateAll(Name.value,Email.value,Phone.value,Password.value,RepeatPassword.value);
-});
-function ValidateAll(name,email,tel,password,repeatPassword){
-    if(name=="" && email=="" && tel=="" && password=="" && repeatPassword==""){
-        Alerta("error","Debes llenar todos los campos.")
-    }else{
-        if(ValidateName(name)){
-            AddValid(Name);
-            if(ValidateTel(tel)){
-                AddValid(Phone);
-                if(ValidateEmail(email)){
-                    AddValid(Email);
-                    if(ValidatePassword(password,repeatPassword)){
-                        AddValid(Password);
-                        AddValid(RepeatPassword);
-                        Alerta("success","Registro exitoso.","rgba(00,20,10,0.2)");
-                    }else{
-                        AddInvalid(Password);
-                        AddInvalid(RepeatPassword);
-                        Alerta("error","Contraseña incorrecta.");
-                    }
-                }else{
-                    AddInvalid(Email);
-                    Alerta("error","Email inválido.");
-                }
-            }else{
-                AddInvalid(Phone);
-                Alerta("error","Teléfono inválido.");
-            }
-        }else{
-            AddInvalid(Name);
-            Alerta("error", "Nombre Inválido.");
-        }
-    }
-
-}
-
-function ValidatePassword(pass1,pass2){
-    if(pass1===pass2 && pass1.length>=5){
-        return true;
-    }else{
-        return false;
-    }
-}
-
-function ValidateEmail(email) {
-	let valid =
-    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
-	if (email.match(valid)) {
-	  return true;
-	} else {
-	  return false;
-	}
-}
-
-function ValidateTel(tel){
-    if (tel.length == 10 && !isNaN(tel)&& tel>1000000000){
-        return true;
-    }else{
-        return false;
-    }
-}
-
-function ValidateName(name){
-    if(typeof name === 'string' && name.length>=3 && name.length<=35){
-        return true;
-    }else{
-        return false;
-    }
-}
-
-function Alerta(estado,mensaje, color="rgba(10,20,30,1)"){
-    Swal.fire({
-        icon: estado,
-        title: mensaje,
-        width: 600,
-        padding: '3em',
-        color: color,
-        background: 'rgba(0,0,0,0.2)'})
-}
-
-function AddInvalid(campo){
-    campo.classList.remove("is-valid");
-	campo.classList.add("is-invalid"); 
-}
-function AddValid(campo){
-    campo.classList.remove("is-invalid");
-    campo.classList.add("is-valid");
-}
-
-key = "user";
+key = "data";
 let lista = [];
 if (localStorage.getItem(key)) {
-    lista = (JSON.parse(localStorage.getItem(key)));
-    lista.forEach(e => {
-    });
+ lista = (JSON.parse(localStorage.getItem(key)));
 }
+id = document.getElementById("sku");
+id.value=(lista.length+23)
+title = document.getElementById("product");
+category = document.getElementById("inputGroupSelect01");
+price = document.getElementById("price");
+Description = document.getElementById("Description");
+urlk = document.getElementById("urlk");
+descuento = document.getElementById("Descuento");
 
-signUpBtn.addEventListener("click", function (e) {
+btn =  document.getElementById("registrar");
+btn.addEventListener("click", function (e) {
     e.preventDefault();
-
-   let check = true;
+   {let check = true;
     if ((isNaN(title.value))&&(title.value.replace(/ /g, "").length>4)){
         title.classList.remove("is-invalid");
         title.classList.add("is-valid");
@@ -175,24 +64,14 @@ signUpBtn.addEventListener("click", function (e) {
         "count":Math.floor(Math.random*1000) },"descuento":descuento.value+"%"}
         lista.push(ContenidoTabla);
         localStorage.setItem(key,JSON.stringify(lista));
+        Swal.fire('Producto Agregado');
+        title.value = '';
+        category.value = '';
+        price.value = '';
+        Description.value = '';
+        urlk.value = '';
+        descuento.value = '';
+
     }   
-
-})
-
-
-
-class usuario {
-        name = "";
-        email = "";
-        password = "";
-        phoneNumber = "";
-        id = 0;
-    constructor (name,email,password,phoneNumber,id)
-    {
-         this.name = name
-         this.email = email
-         this.password = password
-         this.phoneNumber = phoneNumber
-         this.id = id;
-    }//constructor
 }
+})
