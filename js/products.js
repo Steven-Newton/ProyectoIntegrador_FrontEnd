9c2c5a5a90e177fdce0e0460767db1fbfde04a8f
BMRS <brandon25rs@hotmail.com>
2022-10-09 21:25:28 -0500

    A carousel of products has been added

--- js/products.js
+++ js/products.js
@@ -109,3 +109,26 @@
             document.getElementById("SKU"+element.id).style.display = "none";
         }});
     })
+
+    $(document).ready(function () {
+        $("#itemslider").carousel({ interval: 3000 });
+      
+        $(".carousel-showmanymoveone .item").each(function () {
+          var itemToClone = $(this);
+      
+          for (var i = 1; i < 6; i++) {
+            itemToClone = itemToClone.next();
+      
+            if (!itemToClone.length) {
+              itemToClone = $(this).siblings(":first");
+            }
+      
+            itemToClone
+              .children(":first-child")
+              .clone()
+              .addClass("cloneditem-" + i)
+              .appendTo($(this));
+          }
+        });
+      });
+      
